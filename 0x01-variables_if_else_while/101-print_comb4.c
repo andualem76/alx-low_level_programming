d336eb29809eee9e4b66295ff11417e83f9f3234
andualem mamo <andualem7676@gmail.com>
2022-06-05 13:02:27 +0300

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,11 +16,10 @@ for (c = 0; c < 10; ++c)
 {
 for (d = 0; d < 10; ++d)
 {
-for (e = 0; e < 10; ++d)
+for (e = 0; e < 10; ++e)
 {
 if (d > c && e > d)
 {
-
 putchar((c % 10) + '0');
 putchar((d % 10) + '0');
 putchar((e % 10) + '0');
