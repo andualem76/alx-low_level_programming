a6ea858ef2b40f96de8c36b72cf7b7cbd85a5f17
andualem mamo <andualem7676@gmail.com>
2022-06-05 12:39:55 +0300

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; ++c)
+{
+putchar((c % 10) + '0');
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
