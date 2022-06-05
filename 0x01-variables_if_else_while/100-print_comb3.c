327e602d40a7ad0f74fb08fea16e14c943fcb1bb
andualem mamo <andualem7676@gmail.com>
2022-06-05 12:49:43 +0300

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+int d;
+
+for (c = 0; c < 10; ++c)
+{
+for (d = 0; d < 10; ++d){
+
+if(d > c){
+putchar((c % 10) + '0');
+putchar((d % 10) + '0');
+if (c == 8 && d == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+
+}
+}
+
+putchar('\n');
+return (0);
+}
