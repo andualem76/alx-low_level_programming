0676636a75a8067688a75bd7563ee3c99e8d193e
andualem mamo <andualem7676@gmail.com>
2022-06-05 09:58:30 +0300

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float floatType;
 double doubleType;
 char charType;
 
-// sizeof evaluates the size of a variable
+/* sizeof evaluates the size of a variable */
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
