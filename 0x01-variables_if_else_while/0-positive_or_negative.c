ac114385ff85b674e0902551529bba173bd4111d
andualem mamo <andualem7676@gmail.com>
2022-06-05 10:27:38 +0300

    dix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,14 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if(n<0){
+if((n)<0){
 puts("is negative");
 }
-if(n=0)
+else if((n)=0)
 {
 puts("is zero");
 }
-if(n>0){
+else{
 puts("is positive");  
 }
 
