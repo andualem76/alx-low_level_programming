84515ca0f133fff50be7651b6c4b076e09dcfdaf
andualem mamo <andualem7676@gmail.com>
2022-06-05 11:47:34 +0300

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'A'; c <= 'Z'; ++c)
+if ((c != "Q") && (c != "E"))
+putchar(tolower(c));
+
+putchar('\n');
+return (0);
+}
