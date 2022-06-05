78da589b00900cfc572f08a449d36223a81d7c75
andualem mamo <andualem7676@gmail.com>
2022-06-05 12:51:25 +0300

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,9 +13,10 @@ int d;
 
 for (c = 0; c < 10; ++c)
 {
-for (d = 0; d < 10; ++d){
-
-if(d > c){
+for (d = 0; d < 10; ++d)
+{
+if (d > c)
+{
 putchar((c % 10) + '0');
 putchar((d % 10) + '0');
 if (c == 8 && d == 9)
