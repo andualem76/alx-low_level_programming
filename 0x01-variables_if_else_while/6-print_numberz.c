fa2eb83372b799bc76b9d93e63d9416153cbc7c8
andualem mamo <andualem7676@gmail.com>
2022-06-05 12:09:26 +0300

    fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
  * main - Entry point
  *
@@ -10,7 +11,7 @@ int main(void)
 int c;
 
 for (c = 0; c < 10; c++)
-putchar(c);
+putchar(toascii(c));
 
 putchar('\n');
 return (0);
