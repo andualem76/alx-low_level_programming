aafd1662597d7960ceb5380b4a3fdf35c9491f0c
andualem mamo <andualem7676@gmail.com>
2022-06-07 20:02:31 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+printf("_putchar");
+
+return (0);
+}
