de5015645d9bae27c5ca87364425cbbd2182beb4
andualem mamo <andualem7676@gmail.com>
2022-06-07 20:34:16 +0300

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,2 @@
-#include <stdio.h>
-#include "1-alphabet.c"
-#include "0-putchar.c"
 char _putchar(); 
 void print_alphabet();
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-
-return (0);
-}
