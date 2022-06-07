99dee43bcc70318173adbe46be3dff054b81437e
andualem mamo <andualem7676@gmail.com>
2022-06-07 21:07:38 +0300

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,18 +4,19 @@
 
 /**
  * _islower - Entry point
- *
+ * int c - number
  * Description: 'print 10 times alphabey'
  *
  * Return: no
  */
 int _islower(int c)
 {
-if (c == tolower(c)){
-return 1;
+if (c == tolower(c) )
+{
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
