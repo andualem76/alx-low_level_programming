25970a9907a6d43d85e2a71369af575fa0b4e927
andualem mamo <andualem7676@gmail.com>
2022-06-07 20:11:23 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char _putchar()
+{
+putchar('_putchar');
+return 0;
+}
