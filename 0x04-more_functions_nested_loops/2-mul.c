fde13264db51d1f4a08abde983141cd151e90549
andualem mamo <andualem7676@gmail.com>
2022-06-09 18:17:04 +0300

    int mul(int a, int b);

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - writes the character c to stdout
+ * @a: The number to multiply
+ * @b: The number to multiply
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
+
