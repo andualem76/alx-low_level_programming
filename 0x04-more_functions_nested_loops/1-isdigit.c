71b3745da31b210f6d57b5984058e0359426d8ba
andualem mamo <andualem7676@gmail.com>
2022-06-09 18:01:45 +0300

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+if (isdigit(c))
+return (1);
+else{
+return (0);
+}
+
+}
