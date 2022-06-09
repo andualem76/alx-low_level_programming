15ceec768837b84e9849750ad327255d8e90edae
andualem mamo <andualem7676@gmail.com>
2022-06-09 18:24:46 +0300

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _islower - writes the character c to stdout
+ * @a: The number to multiply
+ * @b: The number to multiply
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_numbers(void)
+{
+char i;
+for (i = 0; i < 10; i++){
+putchar(i);
+}
+putchar('\n');
+return 0;
+}
+
